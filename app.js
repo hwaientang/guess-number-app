f83f61e3ebd96a67c5545e164a702940cbb08508
hwaientang <hwaientang@gmail.com>
2021-06-26 13:38:38 +0800

    又新增了好看的css

--- app.js
+++ app.js
@@ -8,17 +8,35 @@ btn.addEventListener('click', function (e) {
     const userval = parseInt(numberInput.value, 10);
 
     if(userval === randomNumber){
-        alert('Your right');
+        // alert('Your right');
+        Swal.fire({
+            title: 'Awesome!!!',
+            text: 'You are Right',
+            icon: 'success',
+            confirmButtonText: 'Cool'
+            })
         return;
     }
 
     if(userval > randomNumber){
-      alert('smaller');
+    //   alert('smaller');
+        Swal.fire({
+        title: 'Error!',
+        text: 'smaller',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+        })
       return;
 }
 
     if(userval < randomNumber){
-     alert('larger');
+    //  alert('larger');
+        Swal.fire({
+        title: 'Error!',
+        text: 'larger',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+        })
      return;
 }
 
